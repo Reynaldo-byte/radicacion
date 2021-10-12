905f11e7145c89179e61fd9767d72d6156e3f752
Reynaldo-byte <64993522+Reynaldo-byte@users.noreply.github.com>
2021-10-12 18:06:00 -0500

    Add files via upload

---
+++ comprobar.js
@@ -0,0 +1,127 @@
+var radical;
+var radicando;
+var raiz;
+var tipo;
+var usuario;
+var cantidad;
+	tipo=Math.floor((Math.random()*2));
+	const audio = document.createElement("audio");
+	audio.preload = "auto";
+	audio.src = "sounds/142.mp3";
+	document.body.appendChild(audio);
+
+//escoge el tipo de ejercio
+function escoger_tipo() {
+
+	audio.play();
+		audio.loop = true;
+	
+
+	
+	switch (tipo){
+		case 0:
+
+			document.querySelector('object').setAttribute("data","html/ejercicio_1.html");
+	
+			document.getElementById("ventana").setAttribute("hidden","");
+			document.getElementById("avanzar").removeAttribute("hidden");
+			break;
+		case 1:
+			document.querySelector('object').setAttribute("data","html/ejercicio_2.html");
+	
+			document.getElementById("ventana").setAttribute("hidden","");
+			document.getElementById("avanzar").removeAttribute("hidden");
+			break;
+		case 2:
+			document.querySelector('object').setAttribute("data","html/ejercicio.html");
+			document.getElementById("ventana").setAttribute("hidden","");
+			document.getElementById("avanzar").removeAttribute("hidden");
+			break;
+
+
+
+
+	}
+}
+
+//genera el ejercicio
+function ejercio() {
+
+	radical=Math.floor((Math.random()*2)+2);
+	raiz=Math.floor(Math.random()*9+1);
+	radicando=Math.pow(raiz,radical);
+
+}
+
+function tipo_1() {
+	ejercio();
+	
+	document.getElementById("imagen").setAttribute("src","../imagenes/raiz_"+radical+".png");
+		document.getElementById("radicando").innerHTML=radicando;
+
+	
+
+
+
+}
+function tipo_2() {
+	ejercio();
+	document.getElementById("radical").innerHTML=raiz;
+	document.getElementById("radicando").innerHTML=radicando
+
+	document.getElementById("imagen").setAttribute("src","../imagenes/raiz_n.png");
+
+
+}
+function tipo_3() {
+
+	ejercio();
+	document.getElementById("raiz").innerHTML=raiz;
+	document.getElementById("imagen").setAttribute("src","../imagenes/raiz_"+radical+".png");
+	
+
+}
+
+
+
+// comprueba los resultados
+
+function comprobar_1(){
+	usuario=document.getElementById("usuario1").value
+	if(raiz==usuario){
+	
+			document.getElementById("body").setAttribute("style","background-image: url('../imagenes/correcto.png');");
+
+	}
+	else{
+		document.getElementById("body").setAttribute("style","background-image: url('../imagenes/error.png');");
+}}
+function comprobar_2() {
+	usuario=document.getElementById("usuario2").value
+
+	if(Math.pow(raiz,usuario)==radicando){
+			
+			document.getElementById("body").setAttribute("style","background-image: url('../imagenes/correcto.png');");
+
+	}
+	else{
+
+		document.getElementById("body").setAttribute("style","background-image: url('../imagenes/error.png');");
+	}
+
+}
+function comprobar_3() {
+	usuario=document.getElementById("usuario3").value
+	if(radicando==usuario){
+		
+			document.getElementById("body").setAttribute("style","background-image: url('../imagenes/correcto.png');");
+		
+	}
+	else{
+			document.getElementById("body").setAttribute("style","background-image: url('/..imagenes/error.png');");
+	}
+	
+}
+
+
+
